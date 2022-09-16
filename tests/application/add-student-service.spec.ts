d4d1a917f51f3d729709c9d1a62f4fef641631b1
João Victor Morais <jv.moraiscb@gmail.com>
2022-09-15 22:08:51 -0300

    :white_check_mark: ensure AddStudentService calls repository correctly

---
+++ tests/application/add-student-service.spec.ts
@@ -0,0 +1,48 @@
+type Student = {
+    id: string
+    name: string
+    age: number
+    password: string
+}
+
+interface AddStudentUseCase{
+    add(student: Student):Promise<Student>
+}
+interface AddStudentRepository{
+    add(student: Student):Promise<Student>
+}
+
+class AddStudentRepositoryMock implements AddStudentRepository{
+    input = null
+    async add(student: Student): Promise<Student> {
+        this.input = student
+        return null
+    }
+}
+
+class AddStudentService implements AddStudentUseCase {
+    constructor(private readonly addStudentRepo: AddStudentRepository) {}
+    async add(student: Student): Promise<Student> {
+        await this.addStudentRepo.add(student)
+        return null
+    }
+}
+
+describe('add-student-service', () =>{
+    it('should call repository with right data', async() =>{
+        const repo = new AddStudentRepositoryMock()
+        const sut = new AddStudentService(repo) // System Under Test
+
+        const fakeStudent: Student = {
+            id: "any_id",
+            name: "any_name",
+            age: 20,
+            password: "any_password"
+
+        }
+
+        await sut.add(fakeStudent)
+
+        expect(repo.input).toEqual(fakeStudent)
+    })
+})
