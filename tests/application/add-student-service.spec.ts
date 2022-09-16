7b66c5c51f04775731bd8d468dd0dced16cc7108
João Victor Morais <jv.moraiscb@gmail.com>
2022-09-15 22:43:38 -0300

    :white_check_mark: ensure AddStudentService encrypts the password before saving it

--- tests/application/add-student-service.spec.ts
+++ tests/application/add-student-service.spec.ts
@@ -5,11 +5,34 @@ type Student = {
     password: string
 }
 
+type AddStudentInputDTO = Omit<Student, 'id'>
+/*
+type AddStudentInputDTO = {
+    name: string
+    age: number
+    password: string
+}
+*/
+
+
 interface AddStudentUseCase{
     add(student: Student):Promise<Student>
 }
 interface AddStudentRepository{
     add(student: Student):Promise<Student>
+}
+interface Encrypter{
+    encrypt(plainText: string): string
+}
+
+class EncrypterMock implements Encrypter{
+    input = ''
+    output = 'encrypter output'
+    
+    encrypt(plainText: string): string{
+        this.input = plainText
+        return this.output
+    }
 }
 
 class AddStudentRepositoryMock implements AddStudentRepository{
@@ -21,24 +44,38 @@ class AddStudentRepositoryMock implements AddStudentRepository{
 }
 
 class AddStudentService implements AddStudentUseCase {
-    constructor(private readonly addStudentRepo: AddStudentRepository) {}
+    constructor(
+        private readonly addStudentRepo: AddStudentRepository,
+        private readonly encrypter: Encrypter
+    ){}
+
     async add(student: Student): Promise<Student> {
-        await this.addStudentRepo.add(student)
+        const encryptedPassword = this.encrypter.encrypt(student.password)
+        
+        const newStudent = {
+            ...student,
+            password: encryptedPassword
+        }
+
+        await this.addStudentRepo.add(newStudent)
         return null
     }
 }
 
 type SutTypes = {
     sut: AddStudentService
+    encrypterMock: EncrypterMock
     repoMock: AddStudentRepositoryMock
 }
 
 const makeSut = (): SutTypes => {
     const repoMock = new AddStudentRepositoryMock()
-    const sut = new AddStudentService(repoMock) // System Under Test
+    const encrypterMock = new EncrypterMock()
+    const sut = new AddStudentService(repoMock, encrypterMock) // System Under Test
     return {
         sut,
-        repoMock
+        repoMock,
+        encrypterMock
     }
 }
 
@@ -51,10 +88,18 @@ const makeFakeStudent = (): Student => ({
 
 describe('add-student-service', () =>{
     it('should call repository with right data', async() =>{
-        const {repoMock, sut} = makeSut()
+        const {repoMock, sut, encrypterMock} = makeSut()
 
         await sut.add(makeFakeStudent())
 
-        expect(repoMock.input).toEqual(makeFakeStudent())
+        expect(repoMock.input).toEqual({...makeFakeStudent(), password: encrypterMock.output})
+    })
+
+    it('should encrypt password before calling the repository', async() =>{
+        const {repoMock, encrypterMock, sut} = makeSut()
+
+        await sut.add(makeFakeStudent())
+
+        expect(repoMock.input.password).toEqual(encrypterMock.output)
     })
 })
